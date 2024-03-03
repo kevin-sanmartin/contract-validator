08efadb8dc1272b9a5ba850a3aae077d1c95aed8
Kévin Sanmartin <kevin.sanmartin@smart-chain.fr>
2024-03-03 17:49:38 +0100

    :sparkles: add Cover page

--- src/components/Pages/Home/CoverPage/index.tsx
+++ src/components/Pages/Home/CoverPage/index.tsx
@@ -1,12 +1,44 @@
 import React from "react";
+import classNames from "classnames";
+
+// Components
+import Typography, { ETypographyType } from "@Components/Elements/Typography";
+import I18n from "@Components/Materials/I18n/I18n";
 
 // Styles
 import classes from "./classes.module.scss";
 
-type IProps = {};
+const CoverPage: React.FC = () => {
+	return (
+		<div className={classes["root"]}>
+			<Typography
+				type={ETypographyType.H3}
+				className={classes["text-align-center"]}>
+				<I18n map="pages.home.cover.conditions" />
+			</Typography>
 
-const CoverPage: React.FC<IProps> = (props) => {
-	return <div className={classes["root"]}></div>;
+			<div className={classes["title-container"]}>
+				<Typography
+					type={ETypographyType.H1}
+					className={classNames(
+						classes["title"],
+						classes["text-align-center"],
+					)}>
+					<I18n map="pages.home.cover.contract" />
+				</Typography>
+
+				<div className={classes["shape"]} />
+
+				<Typography
+					type={ETypographyType.H3}
+					className={classNames(classes["text-align-center"])}>
+					<I18n map="pages.home.cover.prestation" />
+				</Typography>
+			</div>
+
+			<div className={classes["for-flex"]} />
+		</div>
+	);
 };
 
 export default CoverPage;
